27ec81efe2309d3622f59a926bbea7f48c4b3e18
Pishon <159588008+Mini-Pishon@users.noreply.github.com>
2025-04-14 13:30:41 +0200

    Update ACL_Powershell.ps1

--- ACL_Powershell.ps1
+++ ACL_Powershell.ps1
@@ -1,65 +1,65 @@
-# ----------------------------------------- Appuyer sur F5 pour lancer le script -----------------------------------------
-cls  # Nettoyer la console
+# ----------------------------------------- Press F5 to run the script -----------------------------------------
+cls  # Clear the console
 
 function question {
     <#
-    ------------ Ce script permet de nous poser une question et d'exécuter un des deux scripts suivants ------------
+    ------------ This script allows us to ask a question and execute one of the following scripts ------------
     #>
 
-    $compare_txt = ""  # Variable non utilisée, peut être supprimée
-    $question = Read-Host "Bonjour, que souhaitez vous faire :`n
-- Taper 1 pour comparer les groupes AD de deux utilisateurs :`n
-- Taper 2 pour rechercher un groupe AD depuis sa description ou son nom "
+    $compare_txt = ""  # Unused variable, can be removed
+    $question = Read-Host "Hello, what would you like to do:`n
+- Type 1 to compare the AD groups of two users:`n
+- Type 2 to search for an AD group by its description or name"
 
-    If ($question -ne $null) {  # Vérifier si une réponse a été donnée
-        If ($question -eq 1) {  # Si l'utilisateur tape 1
-            Compare_AD_Group  # Appeler la fonction pour comparer les groupes AD
+    If ($question -ne $null) {  # Check if a response was given
+        If ($question -eq 1) {  # If the user types 1
+            Compare_AD_Group  # Call the function to compare AD groups
         }
-        If ($question -eq 2) {  # Si l'utilisateur tape 2
-            search_AD_Group  # Appeler la fonction pour rechercher un groupe AD
+        If ($question -eq 2) {  # If the user types 2
+            search_AD_Group  # Call the function to search for an AD group
         }
     }
 }
 
 function Compare_AD_Group {
     <#
-    ------------ Ce script permet de comparer les groupes AD d'un user2 par rapport à un user1 ------------
+    ------------ This script allows comparing the AD groups of user2 against user1 ------------
     #>
 
-    $User1 = Read-Host "`n `n Quel est l'utilisateur qui possède les droits actuellement (forme : pmaillard) ?"
-    $User2 = Read-Host "Quel est l'utilisateur à qui il manque des droits actuellement (forme : sanne) ?"
+    $User1 = Read-Host "`n `n Who is the user who currently has the rights (format: pmaillard)?"
+    $User2 = Read-Host "Who is the user who is currently missing rights (format: sanne)?"
 
-    # Récupérer les groupes et leurs descriptions pour User1
+    # Retrieve the groups and their descriptions for User1
     $arraytableau = Get-ADPrincipalGroupMembership $User1 | Get-ADGroup -Properties * | Select-Object name, description
-    # Récupérer les groupes et leurs descriptions pour User2
+    # Retrieve the groups and their descriptions for User2
     $arraytableau2 = Get-ADPrincipalGroupMembership $User2 | Get-ADGroup -Properties * | Select-Object name, description
 
-    # Comparer les groupes de User1 et User2 et trouver ceux que User1 a mais pas User2
+    # Compare the groups of User1 and User2 and find those that User1 has but User2 does not
     $arrayComparatif = Compare-Object $arraytableau $arraytableau2 -Property name, description | Where-Object { $_.SideIndicator -eq "<=" }
 
-    Write-Host "`n` `n` `n` `n $User1 a ces groupes que $User2 n'a pas :"
-    $arrayComparatif | Select-Object name, description  # Afficher les groupes que User1 a mais pas User2
+    Write-Host "`n` `n` `n` `n $User1 has these groups that $User2 does not have:"
+    $arrayComparatif | Select-Object name, description  # Display the groups that User1 has but User2 does not
 }
 
 function search_AD_Group {
     <#
-    ------------ Ce script permet de retrouver la présence d'un mot dans la description, le nom d'un groupe AD ------------
+    ------------ This script allows finding the presence of a word in the description or name of an AD group ------------
     #>
 
-    $reponse = Read-Host "`n `n Écrire le mot à chercher dans la description ou le nom des groupes"
-    # Rechercher les groupes dont la description contient le mot
+    $reponse = Read-Host "`n `n Write the word to search in the description or name of the groups"
+    # Search for groups whose description contains the word
     $description = Get-ADGroup -Filter "description -like '*$reponse*'" -Properties * | Select-Object Name, Description
-    # Rechercher les groupes dont le nom contient le mot
+    # Search for groups whose name contains the word
     $nom_de_groupe = Get-ADGroup -Filter "name -like '*$reponse*'" -Properties * | Select-Object Name, Description
 
     if ( ([string]::IsNullOrEmpty($description)) -and ([string]::IsNullOrEmpty($nom_de_groupe)) ) {
-        Write-Host "Il n'y a pas de groupe avec $reponse dans le nom ou la description"
+        Write-Host "There are no groups with $reponse in the name or description"
     } else {
-        Write-Host "Il y a ce(s) groupe(s) avec $reponse dans le nom et la description"
-        $nom_de_groupe  # Afficher les groupes dont le nom contient le mot
-        $description    # Afficher les groupes dont la description contient le mot
+        Write-Host "There are the following group(s) with $reponse in the name and description"
+        $nom_de_groupe  # Display the groups whose name contains the word
+        $description    # Display the groups whose description contains the word
     }
 }
 
-# Appeler la fonction principale pour poser la question à l'utilisateur
+# Call the main function to ask the user the question
 question
